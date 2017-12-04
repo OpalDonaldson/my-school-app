151f67ba957389042a863a498ee17a45eae72d03
Ronique Ricketts <johnoyrick@gmail.com>
2017-12-03 20:29:23 -0500

    added routes

--- school-app/routes/index.js
+++ school-app/routes/index.js
@@ -2,12 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) =>{
   res.render('index', { title: 'Express' });
 });
 
-router.get('/form', (req, res)=>{
-  res.send("HELLO JOHNOY FORM!");
-})
+router.get('/login', (req, res)=>{
+  res.send("login");
+});
+
+router.get('/register', (req, res)=>{
+  res.send("register");
+});
+router.get('/dashboard', (req, res)=>{
+  res.send("dashboard");
+});
+router.get('/overview', (req, res)=>{
+  res.send("overview");
+});
+router.get('/all-activity', (req, res)=>{
+  res.send("all-activity");
+});
 
 module.exports = router;
