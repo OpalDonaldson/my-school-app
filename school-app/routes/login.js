1a62fb0562239ddd213e87052a4e71049f060244
Ronique Ricketts <johnoyrick@gmail.com>
2017-12-11 11:38:39 -0500

    Started login page

--- school-app/routes/login.js
+++ school-app/routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/login', (req, res)=>{
-  res.render('login', { title: 'Login'});
+  res.render('login', { title: 'Login', appName: 'School CMS' });
 });
 
 module.exports = router;
