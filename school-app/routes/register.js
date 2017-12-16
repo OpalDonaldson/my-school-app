13da79c7b6e8d912a840c42f931f3563984f0452
Ronique Ricketts <johnoyrick@gmail.com>
2017-12-16 15:11:58 -0500

    Added regestration fields

--- school-app/routes/register.js
+++ school-app/routes/register.js
@@ -4,5 +4,8 @@ var router = express.Router();
 router.get('/register', (req, res)=>{
   res.render('register', { title: 'Register'});
 });
-
+router.post('/registerusers', (req, res)=>{
+  console.log(req.params.fname);
+  res.send(req.params.fname);
+})
 module.exports = router;
