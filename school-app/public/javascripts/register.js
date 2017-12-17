1bbda908985629a3a17ac212392ac203ca1dae14
Ronique Ricketts <johnoyrick@gmail.com>
2017-12-17 18:23:23 -0500

    Fixed form
    Made form submittable.

---
+++ school-app/public/javascripts/register.js
@@ -0,0 +1,5 @@
+var regForm = document.getElementById('register');
+regData = new FormData(regForm);
+let regSubm = (){
+  console.log(regData);
+}
--- school-app/routes/register.js
+++ school-app/routes/register.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
-
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 router.get('/register', (req, res)=>{
   res.render('register', { title: 'Register'});
 });
 router.post('/registerusers', (req, res)=>{
-  console.log(req.params.fname);
-  res.send(req.params.fname);
+  console.log(req.params);
+  res.send(req.params);
 })
+
 module.exports = router;
