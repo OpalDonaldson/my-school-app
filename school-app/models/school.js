43dab5b8e351af4545c91dca2cdac7a95c07422f
Ronique Ricketts <johnoyrick@gmail.com>
2017-12-24 10:05:21 -0500

    Create DB Posts

--- school-app/models/school.js
+++ school-app/models/school.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var mysql = require('mysql')
-/*
+
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'schooltest'
+  database: 'schoolproject'
 });
-db.connect((err)=>{
-  if(err){
-    throw err;
-  }
-  console.log("Database connected");
-});*/
-const db = "hello";
+
 module.exports = db;
--- school-app/routes/signup.js
+++ school-app/routes/signup.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var db = require('../models/school.js');
 var bcrypt = require('bcrypt');
 var router = express.Router();
 
@@ -6,12 +7,42 @@ router.get('/signup', (req, res)=>{
   res.render('signup', { title: 'Sign-up'});
 });
 router.post('/signup', (req, res)=>{
-  bcrypt.hash(req.body.password, 10, (err, hash)=>{
-    if(err) throw err;
-    console.log(hash);
+  //var user = {};
+  db.connect();
+  db.query('SELECT * FROM `users` WHERE `email` = ?', req.body.email, (err, result, changes)=>{
+    if(err) console.log('This was an error ', err);
+    if(!result.RowDataPackage){
+      let user = {
+        firstname: req.body.fname,
+        lastname: req.body.lname,
+        email: req.body.email,
+        telephonenum: req.body.telephonenum,
+        schoolname: req.body.schoolname,
+        schooladdress: req.body.schooladdress,
+        schooltel: req.body.schooltel,
+        accounttype: req.body.accounttype,
+        password: req.body.password
+      };
+      db.query('INSERT INTO `users` SET ?', user, (error, data, fields)=>{
+        if(error){
+          console.log("Insert Error = ", error);
+        }
+        else{
+          res.render('signup', { warning: "This School Name or email address is already in use."});
+        }
+      });
+    }
+    else{
+      console.log('This already exist', result);
+    }
+
   })
-  console.log(req);
-  res.send(req.params);
+//  bcrypt.hash(req.body.password, 10, (err, hash)=>{
+//    if(err) throw err;
+//    console.log(hash);
+//  })
+
+  res.send(req.body);
 })
 
 
