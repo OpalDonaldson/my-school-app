a27a6e1de6023493a000bfd8ed120ac58fe7aaf1
Ronique Ricketts <johnoyrick@gmail.com>
2018-03-04 08:00:47 -0500

    Auto Update
    Apps updated

--- school-app/routes/signup.js
+++ school-app/routes/signup.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var db = require('../models/school.js');
 var bcrypt = require('bcrypt');
 var router = express.Router();
@@ -6,5 +7,8 @@ var router = express.Router();
 router.get('/signup', (req, res)=>{
   res.render('signup', { title: 'Sign-up'});
 });
-
+router.post('/signup', (req, res)=>{
+  console.log(res.body);
+  res.send(res);
+})
 module.exports = router;
