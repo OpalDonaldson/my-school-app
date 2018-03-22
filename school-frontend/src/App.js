48a775565c0c0a1840658fda38971c7586a3212b
Ronique Ricketts <johnoyrick@gmail.com>
2018-03-21 20:55:01 -0500

    Added React-router
    Attempt to integrate react router to the applicaiton.
    Attempe Basic routing

--- school-frontend/src/App.js
+++ school-frontend/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from "./Home";
+import Signup from "./Signup";
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={Signup} />
+    </div>
+  </Router>
+);
 
 export default App;
---
+++ school-frontend/src/Signup.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Signup</h1>
+        <p>Here is the text about the about us page</p>
+      </div>
+    )
+  }
+}
+
+export default Signup;
